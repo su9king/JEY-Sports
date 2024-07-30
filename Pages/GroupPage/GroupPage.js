b976b8469f3f5d810064b455695819b51b9c9745
GyuHo <gyuho1215@gmail.com>
2024-07-30 19:33:52 +0900

    SW04 imageUpload modify

--- Pages/GroupPage/GroupPage.js
+++ Pages/GroupPage/GroupPage.js
@@ -9,8 +9,8 @@ window.onload = async function() {
         window.location.href = '/WarningPage.html';
     } else {
         document.getElementById('groupName').textContent = `${resources.resources[0]["groupName"]}에 오신 것을 환영합니다!`
-        document.getElementById('groupImage').src = 'JEY_text_icon.jpg';
-        // document.getElementById('groupImage').src = resources.resources[0]["groupIMAGE"];
+        // document.getElementById('groupImage').src = 'JEY_text_icon.jpg';
+        document.getElementById('groupImage').src = `${resources.resources[0]["groupImage"]}`;  ///////////////////////이미지 불러오기
         console.log('회원 인증 성공');
     }
 }
