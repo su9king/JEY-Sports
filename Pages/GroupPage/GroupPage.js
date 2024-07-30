556a951ab6eae6c2f48666fddbe36c8c33182d20
GyuHo <gyuho1215@gmail.com>
2024-07-30 19:13:13 +0900

    SW04 GYU

---
+++ Pages/GroupPage/GroupPage.js
@@ -0,0 +1,16 @@
+window.onload = async function() {
+    const page = 'GroupPage';
+    const data = {userToken: sessionStorage.getItem('userID'), groupToken: sessionStorage.getItem('groupToken')};
+
+    const resources = await certification(page, data);
+
+    if (resources.result == 0) {
+        alert('로그인 후 사용해주세요!');
+        window.location.href = '/WarningPage.html';
+    } else {
+        document.getElementById('groupName').textContent = `${resources.resources[0]["groupName"]}에 오신 것을 환영합니다!`
+        document.getElementById('groupImage').src = 'JEY_text_icon.jpg';
+        // document.getElementById('groupImage').src = resources.resources[0]["groupIMAGE"];
+        console.log('회원 인증 성공');
+    }
+}
--- Pages/PrivatePage/PrivatePage.js
+++ Pages/PrivatePage/PrivatePage.js
@@ -1,12 +1,66 @@
+// const session = require("express-session");
 
-window.onload = function() {
-    certification();
+window.onload = async function() {
+    const page = 'PrivatePage';
+    const data = {userToken: sessionStorage.getItem('userID')};
+    const resources = await certification(page, data);
+    
+    //const resources = {result : 1 , resources : [{groupToken : 1 , groupName : 'GyuChul1'}, {groupToken : 2, groupName : 'GyuChul2'}]};
+
+
+    if (resources.result == 0) {
+        alert('로그인 후 사용해주세요!');
+        window.location.href = '/WarningPage.html';
+    } else if (resources.result == 1) {
+
+        let groups = [];
+        resources.resources.forEach(resource => {
+            groups.push({
+                groupToken: resource.groupToken,
+                groupName: resource.groupName
+            });
+        });
+        
+        createButtons(groups);
+    }
+    
+
 }
 
 
+//////////////////// 버튼 생성 ////////////////////
+function createButtons(groups) {
+    const buttonContainer = document.getElementById('button-container');
+    buttonContainer.innerHTML = '';
+    
+    console.log(groups)
+    groups.forEach((group, index) => {
+        console.log(group);
+        const button = document.createElement('button');
+        button.textContent = `Button ${group['groupName']}`;
+        button.addEventListener('click', () => {
+            alert(`group ${group['groupName']} selected`);
+            sessionStorage.setItem('groupToken', group['groupToken'] );
+            window.location.href = '../GroupPage/GroupPage.html'; 
+        });
+        buttonContainer.appendChild(button);
+    });
+}
+
+
+
+
+//////////////////// 로그아웃, 조직 생성 버튼 이벤트 추가 ////////////////////
 document.addEventListener('DOMContentLoaded', function() {
 
     const logoutBtn = document.getElementById('logout');
     logoutBtn.addEventListener('click', logout); 
     
+    const createGroupBtn = document.getElementById('createGroup');
+    createGroupBtn.addEventListener('click', createGroupPage);
+
 })
+
+function createGroupPage() {
+    window.location.href = '/CreateGroupPage/CreateGroupPage.html'
+}
